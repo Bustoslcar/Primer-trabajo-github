cd20cfe01529448d8ba738fbdb3b42323a161d2c
JaviAltamirano <108379607+JaviAltamirano@users.noreply.github.com>
2022-06-30 00:59:37 -0300

    Revert "Delete segundo intento proyecto 1.py"
    This reverts commit 56f28ed6f9781ce6b106951db5fc71f5c296a446.

---
+++ segundo intento proyecto 1.py
@@ -0,0 +1,14 @@
+#Autor: nd
+#Fecha: 29/06/2022
+#Descripcion: Prueba github
+
+print("Primer programa en python")
+
+
+num1= input("inserte un numero")
+num2= input("inserte otro numero")
+  
+  suma= num1+num2
+  print ("la suma de los numeros es"), sum
+
+  
