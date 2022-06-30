e320421dc6217829520c0bfb4cb0e3dc032aeeec
damianBRAVO <102478250+damianBRAVO@users.noreply.github.com>
2022-06-30 00:33:36 -0300

    Update segundo intento proyecto 1.py

--- segundo intento proyecto 1.py
+++ segundo intento proyecto 1.py
@@ -8,3 +8,12 @@ print("Primer programa en python")
 print ("no se nada D:")
 print (" tampoco yo")
 print("vamo")
+
+n1 = float(input( "Ingrese el primer numero:"))
+n2 = float (input("ingrese el segundo numero:"))
+n3 = float (input("ingrese el tercer numero:"))
+sum = n1 + n2 + n3 
+print ("la suma es: ",sum)
+ esa seria la primera funcion 
+  
+  
