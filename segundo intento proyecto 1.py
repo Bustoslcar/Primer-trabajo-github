3649b450035b0df21ae259788d1b4ad507eee36d
linkygit <108379766+linkygit@users.noreply.github.com>
2022-07-04 02:28:10 -0300

    Update segundo intento proyecto 1.py

--- segundo intento proyecto 1.py
+++ segundo intento proyecto 1.py
@@ -28,3 +28,14 @@ print ("ingrese el tercer número: ")
 n3= float(input())
 resta= n1 - n2 - n3
 print("La resta es: ", resta)
+
+
+print ("vamos a hacer una multiplicacion")
+print("ingrese el primer número: ")
+s1= float(input())
+print("ingrese el segundo número: ")
+s2= float(input())
+print ("ingrese el tercer número: ")
+s3= float(input())
+multiplicacion= s1 + s2 + s3
+print("La multiplicacion es: ", multiplicacion)
