1a5062a96984e2bef6b3d1d90b1080b4737a0011
JaviAltamirano <108379607+JaviAltamirano@users.noreply.github.com>
2022-07-04 02:01:15 -0300

    Update segundo intento proyecto 1.py

--- segundo intento proyecto 1.py
+++ segundo intento proyecto 1.py
@@ -7,3 +7,24 @@ print ("Javier Altamirano")
 print("Margarita Bravo")
 print("Antonini Carla Gisele")
 print("Damian Bravo ")
+
+
+
+
+
+
+print("ingrese el primer número: ")
+n1= float(input())
+print("ingrese el segundo número: ")
+n2= float(input())
+print ("ingrese el tercer número: ")
+n3= float(input())
+suma= n1 + n2 + n3
+print("La suma es: ", suma)
+
+
+
+
+
+
+
