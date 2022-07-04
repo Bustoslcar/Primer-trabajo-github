28e1d5d4312f52afdd86d75ca1f35802456470e7
Bustoslcar <106457289+Bustoslcar@users.noreply.github.com>
2022-07-04 01:27:20 -0300

    Update segundo intento proyecto 1.py

--- segundo intento proyecto 1.py
+++ segundo intento proyecto 1.py
@@ -1,57 +1,8 @@
 #Autor: nd
 #Fecha: 29/06/2022
 #Descripcion: Prueba github
-
-print("Primer programa en python")
-print("no se nada D:")
-print(" tampoco yo")
-print("vamo")
-___________________________________
-n1 = float(input( "Ingrese el primer numero:"))
-n2 = float(input("ingrese el segundo numero:"))
-n3 = float(input("ingrese el tercer numero:"))
-
-sum = n1 + n2 + n3
-print("la suma es: ", sum)
-
-#comentario de margarita
-print("Aquí Carla Antonini presente")
-print("Acá Alfaro Romina")
-#<<<<<<< HEAD
-
-#=======
-print ("Aquí Daniel Varela")
-#>>>>>>> c7a79be2068eb45f4e58ce3f1bc81c2dcca0f004
-
-#comentario de javier
-print ("Aquí Javier Altamirano")
-
-num1 = float(input("Ingrese el primer numero: "))
-num2 = float(input("Ingrese el segundo numero: "))
-resta = num1 - num2
-print("La resta es: ", resta)
-#comentario de Romina
-_________________________________
-print("probando funciones")
-
-def sumar(num1, num2, num3):
-    return(num1+num2+num3)
-print("La suma es :", sumar(1, 2, 3))
-
-def restar(num1, num2):
-    return(num1-num2)
-print("la resta es :", restar(10, 7))
-
-def multiplicar(num1, num2, num3):
-    return(num1*num2*num3)
-print("la multiplicacion :", multiplicar(2, 2, 2))
-
-
-
-
-
-
-
-#cree nueva rama ... javi
+ print ("integrantes del grupo")
+    print ("Micaela Bustos")
+    
 
 
