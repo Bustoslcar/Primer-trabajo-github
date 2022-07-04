e14ec58e3b86bcfe597bf74e2844796c6766a035
JaviAltamirano <108379607+JaviAltamirano@users.noreply.github.com>
2022-07-04 01:57:32 -0300

    Update segundo intento proyecto 1.py

--- segundo intento proyecto 1.py
+++ segundo intento proyecto 1.py
@@ -1,6 +1,14 @@
 #Autor: nd
 #Fecha: 29/06/2022
 #Descripcion: Prueba github
+print ("integrantes del grupo")
+print ("Micaela Bustos")
+print ("Javier Altamirano")
+print("Margarita Bravo")
+print("Antonini Carla Gisele")
+print("Damian Bravo ")
+
+
 
 
 
